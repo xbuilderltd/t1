bdc12902437eb2a0e83c35d6aeb57fe9a06d8719
ccpu <m.doaie@hotmail.com>
2025-07-31 14:17:53 +0100

    fix(release): remove prepareCmd from semantic-release configuration
    * Eliminated the `prepareCmd` line to streamline the release process.
    * This change simplifies the command execution during the release phase.

--- tooling/semantic-release/index.js
+++ tooling/semantic-release/index.js
@@ -21,7 +21,6 @@ export default {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: 'pnpm version ${nextRelease.version} --git-tag-version=false',
         publishCmd: 'pnpm publish --no-git-checks',
       },
     ],
