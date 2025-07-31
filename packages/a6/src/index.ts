69ef6b8179179821b800dc72e87720ee81b7b94d
ccpu <m.doaie@hotmail.com>
2025-07-31 15:01:04 +0100

    fix: update version to 1.0.6
    * Updated the version from 1.0.2 to 1.0.6 for consistency with package versioning.

--- packages/a6/src/index.ts
+++ packages/a6/src/index.ts
@@ -1,5 +1,7 @@
 import * as a5 from '@pixpilot/a5';
 
 export { a5 };
+
+export const name = 'a6';
 export const description = 'A1 package for testing purposes';
-export const version = '1.0.2';
+export const version = '1';
