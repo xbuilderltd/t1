e97a5773c965105731bd7e41846ff3c913e867a0
Your Name <your-email@example.com>
2025-08-01 19:09:11 +0100

    fix: update version string format in index.ts

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,3 +1,3 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws334dvsssdsswwssssv2sssww2ss2ddss2';
+export const version = '4ws334dvdsssdsswwssssv2sssww2ss2ddss2';
