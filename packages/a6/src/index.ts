ac0a73b326fc23b7a580fdf1f59f6891e83e0113
Your Name <your-email@example.com>
2025-08-01 20:44:00 +0100

    ci(release): add --yes flag to nx release command
    * Ensures automatic confirmation for publishing packages.
    * Improves the release workflow by reducing manual intervention.

--- packages/a6/src/index.ts
+++ packages/a6/src/index.ts
@@ -4,4 +4,4 @@ export { a5 };
 
 export const name = 'a6';
 export const description = 'A1 packagse for testing purposes';
-export const version = '3ssssssssddssssssdssssssdqdaaasssswaw';
+export const version = '3ssssssssdssdssssssdssssssdqdaaasssswaw';
