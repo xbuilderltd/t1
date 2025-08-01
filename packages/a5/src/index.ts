02992ec77283ded81133f09ae6f91c552dce5c9f
ccpu <m.doaie@hotmail.com>
2025-08-01 12:17:31 +0100

    fix(a5): correct version string format
    * Fixed the version string in `index.ts` to ensure proper formatting.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,3 +1,3 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws334dvssssswwssv2sssww2s2ddss2';
+export const version = '4ws334dvsssdsswwssv2sssww2s2ddss2';
