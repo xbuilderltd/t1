7ee0d90deea16f5a20b91b5491e8a789b0ded433
Your Name <your-email@example.com>
2025-08-01 20:40:27 +0100

    fix(index): correct version string format in index.ts
    * Updated the version string in `index.ts` to fix a typo.
    * Adjusted the `createRelease` property in `nx.json` to disable GitHub release creation.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,3 +1,3 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws334dvsdsssdsswwssssv2sssww2ss2ddss2';
+export const version = '4wees334dvsdsssdsswwssssv2sssww2ss2ddss2';
