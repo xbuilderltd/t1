eead8da757eb594baa794ee275715ce77eebcda7
Your Name <your-email@example.com>
2025-08-01 20:38:28 +0100

    fix(index): correct typo in description string
    - Changed 'packagse' to 'package' for clarity.

--- packages/a6/src/index.ts
+++ packages/a6/src/index.ts
@@ -3,5 +3,5 @@ import * as a5 from '@pixpilot/nx1';
 export { a5 };
 
 export const name = 'a6';
-export const description = 'A1 package for testing purposes';
+export const description = 'A1 packagse for testing purposes';
 export const version = '3ssssssssddssssssdssssssdqdaaasssswaw';
