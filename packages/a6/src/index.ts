3c3eb6b5eaec626832f038f4062c14e34b407268
Your Name <your-email@example.com>
2025-08-01 19:12:48 +0100

    fix: correct version string format in index.ts

--- packages/a6/src/index.ts
+++ packages/a6/src/index.ts
@@ -4,4 +4,4 @@ export { a5 };
 
 export const name = 'a6';
 export const description = 'A1 package for testing purposes';
-export const version = '3ssssssasssssssdssssssdqdaaasssswaw';
+export const version = '3ssssss2asssssssdssssssdqdaaasssswaw';
