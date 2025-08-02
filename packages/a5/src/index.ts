1e981e05101a25d27c1cede0a34171ed71693d0f
Your Name <your-email@example.com>
2025-08-02 18:42:54 +0100

    fix(index.ts): correct version string in package a5
    * Updated the version string to fix a typo in the package definition.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,4 +1,4 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws33dvsssdsswwssssv2sssww2ddss2';
+export const version = '4ws33dvsssdsswwssssv2sssww2ssddss2';
 // test change
