4e050d60713db6797b76d9938704b60d54fc569c
Your Name <your-email@example.com>
2025-08-02 18:02:34 +0100

    fix(action.yml): add build input to control build step execution
    - Introduced a new input `build` to the action to allow conditional execution of the build step.
    - Updated the `release.yml` workflow to set `build` input to 'false' to skip the build step during deployment.
    - Corrected version string formatting in `index.ts`.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,3 +1,3 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws33dvsssdsswwssssv2sssww2ss2ddss2';
+export const version = '4ws33dvsssdsswwssssv2sssww2ddss2';
