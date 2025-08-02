b1ee4ebc9209925963780063601c9bd73b8d0b29
Your Name <your-email@example.com>
2025-08-02 19:13:33 +0100

    fix(release): update versioning commit message to skip CI
    - Added [skip ci] to the commit message for versioning packages to prevent unnecessary CI runs.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,4 +1,4 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws33dvsssssssssswwsssssv2sssww2ssddss2';
+export const version = '4ws33dvsssssssssssswwsssssv2sssww2ssddss2';
 // test change
