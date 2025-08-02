64abc63a53eab53013ee6baf9277784b549b6ba3
Your Name <your-email@example.com>
2025-08-02 19:54:22 +0100

    ci(release): update workflow to check for existing pre-release mode
    - Changed workflow name from 'Deployment Workflow' to 'Release Workflow'.
    - Added a check to skip entering Changeset pre-release mode if already in it.

--- packages/a5/src/index.ts
+++ packages/a5/src/index.ts
@@ -1,4 +1,4 @@
 export const name = 'a5';
 export const description = 'A1 hpackage for testing purposes';
-export const version = '4ws33dvdd2ssswws2ssddss2';
+export const version = '4ws33dvdd2ssswwss2ssddss2';
 // test change
